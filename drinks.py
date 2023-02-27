75c369b430b6bbb16160d6f04bdef529885ad97b
Nathan Mardanov <S1657228@online.houstonisd.org>
2023-02-27 11:25:41 -0600

    Add project files.

---
+++ drinks.py
@@ -0,0 +1,52 @@
+drink_list = [
+	{
+		"name": "Black",
+		"ingredients": {
+			"coffee": 200
+		}
+	}, {
+		"name": "Americano",
+		"ingredients": {
+			"coffee": 100,
+			"water": 100
+		}
+	}, {
+		"name": "Espresso",
+		"ingredients": {
+			"coffee": 50,
+			"milk": 50
+		}
+	}, {
+		"name": "Doppio",
+		"ingredients": {
+			"coffee": 100,
+			"milk": 50
+		}
+	}, {
+		"name": "Bitch Made",
+		"ingredients": {
+			"coffee": 50,
+			"milk": 100
+		}
+	}, {
+		"name": "Just Milk",
+		"ingredients": {
+			"milk": 100
+		}
+	}, {
+		"name": "Hot Water",
+		"ingredients": {
+			"water": 100
+		}
+	}
+]
+
+drink_options = [
+	{"name": "Black", "value": "black"},
+	{"name": "Americano", "value": "americano"},
+	{"name": "Espresso", "value": "espresso"},
+	{"name": "Doppio", "value": "doppio"},
+	{"name": "Bitch Made", "value": "bm"},
+	{"name": "Just Milk", "value": "jm"},
+	{"name": "Hot Water", "value": "hw"}
+]
