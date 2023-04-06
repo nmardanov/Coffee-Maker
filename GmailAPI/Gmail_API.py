1ef3c985d358fe9bb00e89048bc6cdb86605eaf6
Blubby24 <123764790+Blubby24@users.noreply.github.com>
2023-04-06 15:44:57 -0500

    Update Gmail_API.py

--- GmailAPI/Gmail_API.py
+++ GmailAPI/Gmail_API.py
@@ -67,7 +67,27 @@ def get_contents(pmsg):
     return "No text contents found"
 
 
+def SendEmail(to, cc_mail, body, attachmentspath=None):
+    creds = Credentials(
+        #login information for where email is being sent from
+        username='lamarhstech@outlook.com',
+        password='DiyaIsAmazing!2023!'
+    )
+    account = Account(
+        primary_smtp_address='lamarhstech@outlook.com',
+        credentials=creds,
+        autodiscover=True,
+        access_type=DELEGATE
+    )
 
+    m = Message(
+        account=account,
+        cc_recipients=cc_mail,
+        subject='Information Regarding Your Student',
+        body=HTMLBody(body),
+        to_recipients=[Mailbox(email_address=to)]
+    )
+    m.send()
 
 #Main loop
 def checkMail():
